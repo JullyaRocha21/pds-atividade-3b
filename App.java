849c304a4f58ddcd661037196b3f6b47bbc485ac
Jullya Rocha <144940218+JullyaRocha21@users.noreply.github.com>
2024-12-09 17:26:41 -0300

    Update App.java

--- App.java
+++ App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) {
         // Criando uma instância de Pessoa para cada estuantes
-        Pessoa pessoa = new Pessoa("Jullya", 17);
+        Pessoa pessoa = new Pessoa("Jullya Rocha da Costa Lima", 17);
 
         // Exibindo as informações da estudante
         pessoa.exibirInformacoes();
